efc74e71478c3ae098c97356dd25808e241fa11c
Brayan Diaz C <brayandiazc@gmail.com>
2023-07-19 10:23:15 -0500

    feat: Seed configurado

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Image.create!(
+  url: 'https://i0.wp.com/chocale.cl/wp-content/uploads/2020/06/perro-lipigas.jpg?fit=1000%2C666&ssl=1',
+  imageable: Pet.create!(
+    name: 'perrito lipigas',
+    race: 'quiltro',
+    age: 12,
+    gender: 'M',
+    user: User.create!(
+      email: 'owner1@gmail.com',
+      password: 'asdasdasd',
+      password_confirmation: 'asdasdasd',
+      name: 'el dueñaso',
+      phone: '+56983323932'
+    )
+  )
+)
+
+Image.create!(
+  url: 'https://picsum.photos/id/237/200/300',
+  imageable: Pet.create!(
+    name: 'perrito negro',
+    race: 'perrito',
+    age: 12,
+    gender: 'F',
+    user: User.create!(
+      email: 'owner2@gmail.com',
+      password: '123456',
+      password_confirmation: '123456',
+      name: 'la dueña',
+      phone: '+56983323931'
+    )
+  )
+)
+
+Image.create!(
+  url: 'https://static.vecteezy.com/system/resources/previews/013/777/225/original/tired-man-from-work-illustration-on-white-background-freevector.jpg',
+  imageable: User.create!(
+    email: 'owner@gmail.com',
+    password: 'asdasdasd',
+    password_confirmation: 'asdasdasd',
+    name: 'El jefazo',
+    phone: '9825911024'
+  )
+)
